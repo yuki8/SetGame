3d004eb39fa9ae08637db7b46bf134ea1446e0a0
minipower <ymuto@hotmail.com>
2023-02-11 14:04:21 -0800

    add card view and apply Aspect VGrid view

--- SetGame/ContentView.swift
+++ SetGame/ContentView.swift
@@ -10,14 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var game: SetGame
     var body: some View {
-        VStack {
-            ForEach(game.model.cards) { card in
-                Text("\(card.id)")
-            }
+        AspectVGrid(items: game.model.cards, aspectRatio: 2/3) { card in
+                CardView(card: card)
+                .padding(3)
         }
+        .foregroundColor(.gray)
         .padding()
     }
 }
+
+
+struct CardView: View {
+    let card: Set.Card
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10).foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 10).strokeBorder(lineWidth: 3)
+            Text("\(card.id)")
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
